0f6f5acf576f25693acd03e58f4e4ff25f2d9083
Mathias Olesen <math09s0@stud.kea.dk>
2023-05-30 18:48:17 +0200

    indhold-css rettelser/justeringer

--- src/scripts/materialer_scroll.js
+++ src/scripts/materialer_scroll.js
@@ -3,8 +3,8 @@ const arrow2 = document.querySelector('#arrow2');
 
 /* Scroller venstre/højre når venstre/højre pil trykkes */ 
 arrow1.onclick = function () {
-  document.querySelector('.scrolling_wrapper').scrollLeft -= 600;
+  document.querySelector('.scrolling_wrapper').scrollLeft -= 270;
 };
 arrow2.onclick = function () {
-  document.querySelector('.scrolling_wrapper').scrollLeft += 600;
+  document.querySelector('.scrolling_wrapper').scrollLeft += 270;
 };
