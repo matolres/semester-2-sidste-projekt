50ed892a98f20a910e75ec97282dea9b022468e8
Mathias Olesen <math09s0@stud.kea.dk>
2023-05-31 16:05:44 +0200

    java

--- src/scripts/materialer_scroll.js
+++ src/scripts/materialer_scroll.js
@@ -2,9 +2,12 @@ const arrow1 = document.querySelector('#arrow1');
 const arrow2 = document.querySelector('#arrow2');
 
 /* Scroller venstre/højre når venstre/højre pil trykkes */ 
+document.addEventListener("DOMContentLoaded", documentReady)
+function documentReady() {
 arrow1.onclick = function () {
   document.querySelector('.scrolling_wrapper').scrollLeft -= 270;
 };
 arrow2.onclick = function () {
   document.querySelector('.scrolling_wrapper').scrollLeft += 270;
 };
+}
